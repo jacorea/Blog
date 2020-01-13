1aa73902bd47e66cb59244b1eb13da501a3a1190
jcorea <jacorea410@gmail.com>
2020-01-13 11:22:32 -0800

    Add Axios HTTP service

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +48,7 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
