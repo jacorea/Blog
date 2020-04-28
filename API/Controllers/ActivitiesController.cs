8b9fb0945ee413a559e49d5a78d79af5cf278feb
jcorea <jacorea410@gmail.com>
2020-04-28 10:49:32 -0700

    add Activities to API controller to handle routing to Details of one activity.

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Domain;
 using Application.Activities;
+using System;
 
 namespace API.Controllers
 {
@@ -21,6 +22,12 @@ namespace API.Controllers
         public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query{Id = id});
         }
     }
 }
--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,7 +1,36 @@
+using MediatR;
+using Domain;
+using System;
+using System.Collections.Generic;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Application;
+using System.Threading;
+
+
 namespace Application.Activities
 {
     public class Details
     {
-        
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+                return activity;
+            }
+        }
     }
 }
+
