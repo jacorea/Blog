4cd23106599e5c054c88ed425ef389a536bbc897
jcorea <jacorea410@gmail.com>
2020-04-28 16:28:35 -0700

    Created basic ActivityDashboard and ActivityList components for dashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Icon, List } from 'semantic-ui-react';
+import { Header, Icon, List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 
+
+//imported components
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 const App =() => {
 
@@ -17,20 +21,12 @@ const App =() => {
   }, [])
 
   return (
-    <div>
-      <Header as='h2'>
-      <Icon name='users' />
-        <Header.Content>Reactivities</Header.Content>
-      </Header>
-        <ul>
-          <List>
-
-          {activities.map((activity: any) => (
-              <List.Item key={activity.id}>{activity.title}</List.Item>
-              ))}
-          </List>
-        </ul>
-    </div>
+    <Fragment>  
+      <NavBar />
+        <Container style ={{marginTop: '6em'}}>
+          <ActivityDashboard activities = {activities} />
+        </Container>
+    </Fragment>
   );
 } 
 
---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Grid, List } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
+
+interface IProps {
+    activities: IActivity[]
+}
+
+const ActivityDashboard: React.FC<IProps> = ({activities}) => {
+  return (
+    <Grid>
+        <Grid.Column width={10} >
+            <ActivityList activities={activities} />
+
+            {/* <List>
+                {activities.map((activity: any) => (
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
+                ))}
+            </List> */}
+        </Grid.Column>
+    </Grid>
+  )
+}
+
+export default ActivityDashboard;
